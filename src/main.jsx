4dacb582c1738a49da12fcf0d1a932c0157711e8
Shan-N <shantanu.neve2000@gmail.com>
2024-10-20 15:08:29 +0530

    landing and event page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Permission from './components/Permission'
+import Landing from './components/Landing'
+import EventDetails from './components/Event'
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Landing />
   
+  },
+  {
+    path: "/event/:id",
+    element: <EventDetails />
   },
   {
     path:'/permission',
